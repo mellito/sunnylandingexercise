8ed5befea9e5896906aef63b78804e76df3acd60
mellito <dayanalexismanrique@hotmail.com>
2022-01-30 21:29:09 -0500

    changing path of all img

--- docs/data.js
+++ docs/data.js
@@ -1,20 +1,20 @@
 const testimonial = [
   {
-    Image: "/images/image-emily.jpg",
+    Image: "/docs/images/image-emily.jpg",
     testimonial:
       "We put our trust in Sunnyside and they delivered, making sure our needs were met and deadlines were always hit",
     name: "Emily R.",
     profession: "Marketing Director",
   },
   {
-    Image: "/images/image-thomas.jpg",
+    Image: "/docs/images/image-thomas.jpg",
     testimonial:
       "Sunnyside’s enthusiasm coupled with their keen interest in our brand’s success made it a satisfying and enjoyable experience.",
     name: "Thomas S.",
     profession: "Chief Operation Officer",
   },
   {
-    Image: "/images/image-jennie.jpg",
+    Image: "/docs/images/image-jennie.jpg",
     testimonial:
       "Operating Officer Incredible end result! Our sales increased over 400% when we worked with Sunnyside. Highly recommended!",
     name: "Jennie F.",
